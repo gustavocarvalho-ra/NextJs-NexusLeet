61e9fcfd2b66540e344e96ae2d79c524a92a330e
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-27 00:20:44 -0300

    feat: change link href

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <div className="w-screen min-h-6 bg-amber-100">
-      <Link href={"/src/app/page.tsx"} className="text-amber-600">Nexus Leet</Link>
+    <div className="w-full h-20 bg-amber-400">
+      <Link href={"/"} className="text-amber-600">Nexus Leet</Link>
     </div>
   )
 }
--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -1,9 +1,11 @@
+import Header from '../components/Header';
 import LoginForm from '../components/login/LoginForm';
 import RegisterForm from '../components/login/RegisterForm';
 
 export default function MyAccont() {
   return (
-    <div className="w-screen h-screen flex">
+    <div className="w-screen h-screen flex flex-col">
+      <Header />
       <LoginForm />
       <RegisterForm />
     </div>
