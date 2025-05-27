71ed6ec22bd57beb3838e59d65f79333b212236a
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-26 22:49:24 -0300

    feat: add import link

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import Link from "next/link"
+
 export default function Header() {
   return (
     <div className="w-screen">
-
+      <Link href={"/src/app/page.tsx"}></Link>
     </div>
   )
 }
