c0f4ada01cc10da631d72eb80c405a77f0b67e9b
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-26 22:53:03 -0300

    style: add color test

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <div className="w-screen">
-      <Link href={"/src/app/page.tsx"}></Link>
+    <div className="w-screen min-h-6 bg-amber-100">
+      <Link href={"/src/app/page.tsx"} className="text-amber-600">Nexus Leet</Link>
     </div>
   )
 }
