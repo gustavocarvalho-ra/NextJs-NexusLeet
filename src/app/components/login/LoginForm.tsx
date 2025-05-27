742c9e3dd6570c3c0624850ae7ddfe2e926633e5
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-27 15:46:17 -0300

    feat: start styles input

--- src/app/components/login/LoginForm.tsx
+++ src/app/components/login/LoginForm.tsx
@@ -27,8 +27,8 @@ export default function LoginForm() {
 
   return (
     <div className="w-full h-full bg-amber-100 flex items-center justify-center">
-      <div className="border-double rounded-lg p-3 w-xl bg-blue-700 h-5/12 ">
-        <h1>Login</h1>
+      <div className="border-double rounded-lg p-3 w-1/2 bg-amber-950 h-5/12 flex flex-col justify-center">
+        <h1 className="flex justify-center text-cyan-200 text-3xl">Login</h1>
         <div className="h-full w-full">
           <form onSubmit={handleLogin} className="gap-5 flex flex-col items-center h-4/5">
             <input 
@@ -38,13 +38,16 @@ export default function LoginForm() {
               onChange={e => setEmail(e.target.value)}
               className="bg-red-700 rounded-sm p-2 h-2/13 w-2/3 border-2 border-amber-50"
             />
-            <input 
-              placeholder="Senha" 
-              type="password" 
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              className="bg-red-700 rounded-sm p-2 h-2/13 w-2/3 border-2 border-amber-50"
-            />
+            <div className="">
+              <span></span>
+              <input 
+                placeholder="Senha" 
+                type="password" 
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className="border-b-sky-100 rounded-sm p-2 h-2/13 w-2/3 border-2 border-amber-50"
+              />
+            </div>
             <button type="submit" className="cursor-pointer">
               Entrar
             </button>
