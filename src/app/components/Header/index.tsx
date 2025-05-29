c28bca82816e97d0064de1b04839a7dda403c317
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-28 23:59:22 -0300

    feat: add input

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -6,6 +6,7 @@ export default function Header() {
   return (
     <div className="w-full h-20 bg-amber-400">
       <Link href={"/"} className="text-amber-600">Nexus Leet</Link>
+      <div><input /></div>
     </div>
   )
 }
