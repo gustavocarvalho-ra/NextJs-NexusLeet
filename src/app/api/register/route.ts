9c9a25c95d0cf8d98193ba68c91394bad2a47df3
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-29 23:31:34 -0300

    feat: test route

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
   const { name, email, password } = await req.json();
 
   if (!email || !password) {
-    return NextResponse.json({ error: "Email e senha são obrigatórios "}, { status: 400 })
+    return NextResponse.json({ error: "Dados inválidos"}, { status: 400 })
   };
 
   const existingUser = await prisma.user.findUnique({
