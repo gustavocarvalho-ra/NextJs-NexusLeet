8b101c0f552300321cf2f83c4d17b0cf8941b724
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-29 23:39:12 -0300

    feat: add try catch

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,30 +3,44 @@ import { prisma } from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const { name, email, password } = await req.json();
+  try {
 
-  if (!email || !password) {
-    return NextResponse.json({ error: "Dados inválidos"}, { status: 400 })
-  };
-
-  const existingUser = await prisma.user.findUnique({
-    where: { email },
-  });
+    const { name, email, password } = await req.json();
   
-  if (existingUser) {
-    return NextResponse.json({ error: "Email já cadastrado" }, { status: 400})
-  };
-
-  const hashedPassword = await hash(password, 10)
-
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-      role: 'USER',
-    }
-  });
-
-  return NextResponse.json({ message: "Usuário cadastrado com sucesso", user }, {status: 201});
+    if (!name || !email || !password) {
+      return NextResponse.json({ error: "Dados inválidos"}, { status: 400 })
+    };
+  
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    
+    if (existingUser) {
+      return NextResponse.json({ error: "Email já cadastrado" }, { status: 400})
+    };
+  
+    const hashedPassword = await hash(password, 10)
+  
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role: 'USER',
+      },
+    });
+  
+    return NextResponse.json({ 
+      message: "Usuário cadastrado com sucesso",
+      user: {
+        id: user.id,
+        name: user.name,
+        Email: user.email,
+        role: user.role,
+      }
+    }, {status: 201});
+  } catch (error) {
+    console.error("Erro no cadastro", error);
+    return NextResponse.json({ error: "Erro interno do servidor" }, { status: 500});
+  }
 }
