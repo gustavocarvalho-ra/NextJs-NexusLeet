d9030e3ece8749bdf7f1aece819cff885856a46a
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-30 18:54:12 -0300

    feat: add defaultSession

--- src/app/types/next-auth.d.ts
+++ src/app/types/next-auth.d.ts
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
@@ -8,8 +8,8 @@ declare module "next-auth" {
       name?: string | null;
       email?: string | null;
       image?: string | null;
-      role?: string
-    };
+      role?: string;
+    } & DefaultSession["user"];
   }
 
   interface User {
