9df9280d355051e35b43517ea764ccf040bd89f4
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-31 22:51:38 -0300

    feat: add session user name in header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,12 +1,29 @@
-"use client"
+// "use client"
+import { getServerSession } from 'next-auth';
 
+import { authOptions } from '@/app/lib/auth';
 import Link from "next/link"
 
-export default function Header() {
+export default async function Header() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return (
+      <div className="w-full h-20 bg-amber-400 flex items-center justify-evenly">
+        <Link href={"/"} className="text-amber-600">Nexus Leet</Link>
+        <input />
+        <Link href={"/my-account"}>Login</Link>
+      </div>
+    )
+  }
+
   return (
     <div className="w-full h-20 bg-amber-400">
       <Link href={"/"} className="text-amber-600">Nexus Leet</Link>
-      <div><input /></div>
+      <div>
+        <input />
+        <p>Bem vindo, {session.user?.name}!</p>
+      </div>
     </div>
   )
 }
