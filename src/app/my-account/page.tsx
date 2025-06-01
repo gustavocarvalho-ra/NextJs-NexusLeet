26036afba38c963321054113f03c3f3a2e96cbd4
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-31 23:42:51 -0300

    feat: file change

--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -2,16 +2,25 @@ import Header from '../components/Header';
 import LoginForm from '../components/login/LoginForm';
 import RegisterForm from '../components/login/RegisterForm';
 
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../lib/auth';
+import { redirect } from 'next/navigation';
+
 
 export default async function MyAccont() {
+  const session = await getServerSession(authOptions)
 
-  return (
-    <div className="w-screen h-screen flex flex-col">
-      <Header />
-      <div className="flex w-screen h-screen">
-        <LoginForm />
-        <RegisterForm />
+  if (!session) {
+    return (
+      <div className="w-screen h-screen flex flex-col">
+        <Header />
+        <div className="flex w-screen h-screen">
+          <LoginForm />
+          <RegisterForm />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  redirect("/my-account/user");
 }
