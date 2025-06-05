b525287cb36e086e3726f15488aa308c20cd05ae
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-05 20:39:58 -0300

    feat: add redirect

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="w-full min-h-4 text-shadow-amber-200">
+    <div className="w-full min-h-4 text-shadow-amber-200 flex justify-center gap-8 text-sm font-semibold">
       <Link href={"/"}>Inicio</Link>
       <Link href={"/my-account/user"}>Conta</Link>
     </div>
--- src/app/my-account/user/page.tsx
+++ src/app/my-account/user/page.tsx
@@ -1,16 +1,13 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/lib/auth';
 import Header from '@/app/components/Header';
-// import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default async function MyAccontUser() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    return (
-      <h1>erro de sessão</h1>
-    )
-    // redirect("/usuarionaologado");
+    redirect("/my-account");
   }
 
   return (
