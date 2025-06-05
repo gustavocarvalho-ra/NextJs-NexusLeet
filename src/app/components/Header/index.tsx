59945a041abf1d00869936279a8db5c8cb545a49
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-05 20:34:07 -0300

    style: changed flex

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -13,20 +13,23 @@ export default async function Header() {
 
   if (!session) {
     return (
-      <div className="w-full min-h-26 bg-black flex items-center justify-evenly">
-        <Link href={"/"} className="text-amber-600 font-black text-4xl">
-          Nexus Leet
-        </Link>
+      <div className="w-screen min-h-26 bg-black flex flex-col justify-center">
+        <div className="w-full flex justify-evenly">
+          <Link href={"/"} className="text-amber-600 font-black text-4xl">
+            Nexus Leet
+          </Link>
 
-        <input placeholder='Buscar produto' className='bg-cyan-200 w-1/3 h-8 pl-2.5 text-black font-medium rounded-sm'/>
+          <input placeholder='Buscar produto' className='bg-cyan-200 w-1/3 h-8 pl-2.5 text-black font-medium rounded-sm'/>
 
-        <Link href={"/my-account"} className='flex items-center'>
-          <FaRegUser className='text-3xl' />
-          <div className='flex flex-col pl-2 font-extralight text-[12px]'>
-            <p><span className='font-black'>Olá! Faça login</span></p>
-            <p >Ou Cadastre-se</p>
-          </div>
-        </Link>
+          <Link href={"/my-account"} className='flex items-center'>
+            <FaRegUser className='text-3xl' />
+            <div className='flex flex-col pl-2 font-extralight text-[12px]'>
+              <p><span className='font-black'>Olá! Faça login</span></p>
+              <p >Ou Cadastre-se</p>
+            </div>
+          </Link>
+        </div>
+        <Nav />
       </div>
     )
   }
