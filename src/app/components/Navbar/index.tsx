cffe45806dbe1e95ab5ec8aa717298806c6660af
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-05 20:27:10 -0300

    style: add width and heigth

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div>
+    <div className="w-full min-h-4 text-shadow-amber-200">
       <Link href={"/"}>Inicio</Link>
       <Link href={"/my-account/user"}>Conta</Link>
     </div>
