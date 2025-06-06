0ba8bb002e1f1a567504b1168805924d8c08a9f1
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-05 23:46:25 -0300

    style: changed style header

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="w-full min-h-4 text-shadow-amber-200 flex justify-center gap-8 text-sm font-semibold">
+    <div className="w-full min-h-4 text-shadow-amber-200 flex justify-center gap-8 text-sm font-semibold pt-1.5">
       <Link href={"/"}>Inicio</Link>
       <Link href={"/my-account/user"}>Conta</Link>
     </div>
