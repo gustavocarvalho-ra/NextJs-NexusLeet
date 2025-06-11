039ccb2042f988db612e42b9b53c242fdc97947b
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-10 23:27:58 -0300

    feat: deleted root *

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="w-full min-h-4 text-shadow-amber-200 flex justify-center gap-8 text-sm font-semibold pt-1.5">
-      <Link href={"/"}>Inicio</Link>
-      <Link href={"/my-account/user"}>Conta</Link>
+    <div className="w-full min-h-4 flex justify-center gap-8 text-sm font-semibold pt-1.5">
+      <Link className="text-(--color-sec) hover:bg-(--hover-pr)" href={"/"}>Inicio</Link>
+      <Link className="text-(--color-sec) hover:bg-(--hover-pr)" href={"/my-account/user"}>Conta</Link>
     </div>
   )
 }
