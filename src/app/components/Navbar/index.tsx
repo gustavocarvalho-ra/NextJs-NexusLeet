f4718fae639aa27b1df63d5c820ce81b043187b5
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-11 23:08:58 -0300

    feat: add transition

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Nav() {
   return (
     <div className="w-full min-h-4 flex justify-center gap-8 text-sm font-semibold pt-1.5">
-      <Link className="text-(--color-sec) hover:text-(--hover-pr)" href={"/"}>Inicio</Link>
-      <Link className="text-(--color-sec) hover:text-(--hover-pr)" href={"/my-account/user"}>Conta</Link>
+      <Link className="text-(--color-sec) hover:text-(--hover-pr) duration-300 ease-out" href={"/"}>Inicio</Link>
+      <Link className="text-(--color-sec) hover:text-(--hover-pr) duration-300 ease-out" href={"/my-account/user"}>Conta</Link>
     </div>
   )
 }
