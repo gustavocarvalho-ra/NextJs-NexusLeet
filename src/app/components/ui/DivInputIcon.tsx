784295c2371d9019a9d995c163826962dfc17b48
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-17 21:31:17 -0300

    fix: correct background class syntax in DivInputIcon component

--- src/app/components/ui/DivInputIcon.tsx
+++ src/app/components/ui/DivInputIcon.tsx
@@ -8,7 +8,7 @@ interface DivInputIconProps extends React.InputHTMLAttributes<HTMLInputElement> 
 const DivInputIcon = React.forwardRef<HTMLInputElement, DivInputIconProps>(
   ({ className, icon: IconComponent, ...props }, ref) => {
     return (
-      <div className="bg(--color-sec) rounded-sm p-2 h-2/13 w-2/3 border-2 flex items-center">
+      <div className="bg-(--color-sec) rounded-sm p-2 h-2/13 w-2/3 border-2 flex items-center">
         <Input
           ref={ref}
           className={className}
