f4321a97374a79e00084c12929905101869f7edb
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-20 00:08:21 -0300

    style: changed bg color

--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -14,7 +14,7 @@ export default async function MyAccont() {
     return (
       <div className="w-screen h-screen flex flex-col">
         <Header />
-        <div className="flex w-screen h-screen bg-(--bg-test)">
+        <div className="flex w-screen h-screen bg-(--bg)">
           <LoginForm />
           <RegisterForm />
         </div>
