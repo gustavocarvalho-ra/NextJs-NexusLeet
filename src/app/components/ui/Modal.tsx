04e6103d370dae9a4b210c1b63deb645e1d679bb
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-24 21:01:16 -0300

    feat: test modal

--- src/app/components/ui/Modal.tsx
+++ src/app/components/ui/Modal.tsx
@@ -1,17 +1,30 @@
+// "use client"
 
-interface ModalProps {
-  isOpen?:void;
-  onClose?: () => void;
-}
+import { useState } from "react";
 
-export default function Modal({isOpen, onClose}: ModalProps) {
-  if (!isOpen) return null;
+// interface ModalProps {
+//   isOpen?: boolean;
+//   onClose?: () => void;
+// }
+
+export default function Modal() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpen(!isOpen);
+  };
+
 
   return(
-    <button className="text-white" onClick={isOpen}>
-      Abrir modal
-      <div className="w-screen h-screen bg-amber-100" onClick={onClose}></div>
-    
-    </button>
+    <div>
+      <button className="text-white cursor-pointer" onClick={handleOpenModal}>
+        Abrir modal
+      
+      </button>
+
+        <div className="w-screen h-screen absolute bg-amber-100" onClick={onClose}>
+
+        </div>
+    </div>
   )
 }
