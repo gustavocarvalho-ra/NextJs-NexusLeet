12a35126bd67a16ad02939d3e51af48d61d705d9
GustavoCR021 <gustavoc1922@gmail.com>
2025-06-26 22:48:04 -0300

    feat: update user greeting in Header component to include modal for profile actions

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -43,16 +43,20 @@ export default async function Header() {
       
       <input placeholder='Buscar produto' className='bg-(--color-sec) w-1/3 h-8 pl-2.5 text-black font-medium rounded-sm'/>
 
-      <div className='flex text-(--text-amber)'>
-          <FaRegUser className='text-2xl' />
-          {session.user?.name}!
+      <div className='flex gap-1.5 text-(--text-amber)'>
         <Modal 
-          modalContent={
-            <div className='h-5 w-2'>
-              <Link href={"/my-account/user"}>Meu perfil</Link>
 
-              <ButtonSignOut />
-            </div>
+
+          modalContent={
+            <>
+              <FaRegUser className='text-2xl' />
+              <p>`${session.user?.name}`</p>
+              <div className='h-5 w-2'>
+                <Link href={"/my-account/user"}>Meu perfil</Link>
+
+                <ButtonSignOut />
+              </div>
+            </>
           }/>
       </div>
     </div>
