6feab03c881dba3d7afa027cec4e1386d30a28d6
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-05 00:10:21 -0300

    feat: test image

--- src/app/my-account/user/page.tsx
+++ src/app/my-account/user/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/lib/auth';
 import Header from '@/app/components/Header';
 import { redirect } from 'next/navigation';
+import Image  from 'next/image';
 
 export default async function MyAccontUser() {
   const session = await getServerSession(authOptions);
@@ -16,8 +17,8 @@ export default async function MyAccontUser() {
       <div className='w-full h-full flex justify-center'>
 
         <div className="w-1/2 bg-amber-400">
-          <div>
-            
+          <div className='w-20 h-20 relative rounded-full flex justify-center overflow-clip items-center bg-amber-100'>
+            <Image className='absolute' src={"/iduser.jpeg"} width={150} height={150} alt='User Image' />
           </div>
           <h1>Minha Conta</h1>
           <p>Bem vindo, {session.user?.name || session.user?.email}!</p>
