3dad46818d98dd846c2113bb52547a630c2710a6
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-07 23:58:00 -0300

    feat: test card

---
+++ src/app/components/ui/CardUser.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CardUser() {
+  return (
+    <div className="w-1xs h-16 bg-amber-200 flex justify-center">
+
+      <h1>test card</h1>
+    </div>
+  )
+}
