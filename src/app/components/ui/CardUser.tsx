13d40716cb04a50201f219f85c3f8660350e1390
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-08 00:04:37 -0300

    style: changed width

--- src/app/components/ui/CardUser.tsx
+++ src/app/components/ui/CardUser.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CardUser() {
   return (
-    <div className="w-1xs h-16 bg-amber-200 flex justify-center">
+    <div className="w-3/12 h-16 bg-amber-200 flex justify-center">
 
       <h1>test card</h1>
     </div>
