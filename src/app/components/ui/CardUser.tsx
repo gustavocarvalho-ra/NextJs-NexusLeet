f2310127af9d5a0eb5ba62cca108c7195bfbbcad
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-10 23:27:41 -0300

    style]: changed text size

--- src/app/components/ui/CardUser.tsx
+++ src/app/components/ui/CardUser.tsx
@@ -13,7 +13,7 @@ export default function CardUser({icon, TextStr, AboutStr}: CardInt) {
     <div className="w-1/3 h-36 bg-(--mod) flex flex-col justify-center rounded-lg p-3.5">
       <h1 className="font-black h-1/4 text-xl">{icon}</h1>
       <h2 className="font-semibold h-1/4">{TextStr}</h2>
-      <h3 className="font-extralight h-1/2">{AboutStr}</h3>
+      <h4 className="font-extralight h-1/2 text-sm">{AboutStr}</h4>
     </div>
   )
 }
