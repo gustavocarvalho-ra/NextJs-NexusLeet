6916b7ae7e1985f1567d826458417d69824de575
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-14 20:19:59 -0300

    feat: add main page

---
+++ src/app/pag/main/page.tsx
@@ -0,0 +1,21 @@
+// import Footer from "./components/Footer";
+// import MyAccont from "./my-account/page";
+import Card from './../../components/ui/Product/Card';
+import Header  from '@/app/components/Header';
+
+export default function Main() {
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <Header />
+      <h1>Tela inicial</h1>
+      <div className='w-11/12 h-screen flex flex-wrap gap-4'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
