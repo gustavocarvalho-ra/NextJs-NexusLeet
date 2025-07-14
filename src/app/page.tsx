22081856195899bf24ee4c5b507b35573deb9bc7
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-14 20:20:53 -0300

    feat: test main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-// import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Card from "./components/ui/Product/Card";
-// import MyAccont from "./my-account/page";
+import Main from "./pag/main/page";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col">
-      <Header />
-      <h1>Tela inicial</h1>
-      <Card />
-      {/* <MyAccont /> */}
-      {/* <Footer /> */}
-    </div>
+    <Main />
   );
 }
