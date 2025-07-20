ed3313e5884436eb869abb7dc5738ced71474920
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-20 20:35:13 -0300

    feat: changed sizes

--- src/app/components/ui/Product/Card.tsx
+++ src/app/components/ui/Product/Card.tsx
@@ -1,9 +1,10 @@
 
 
-export default function Card() {
+export default function Card(Name, Price, Type) {
   return (
-    <div className="bg-amber-600 w-full h-full">
-
+    <div className="bg-amber-600 w-full h-full p-2 flex items-center flex-col gap-2">
+      <div className="bg-amber-100 w-[80%] h-3/4"></div>
+      <div className="bg-amber-100 w-full h-1/4"></div>
     </div>
   )
 }
