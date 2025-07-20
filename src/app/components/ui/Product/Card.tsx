9a718ccd196435919d761afcd0a6e8dd47c4a305
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-20 20:48:49 -0300

    style: changed width size

--- src/app/components/ui/Product/Card.tsx
+++ src/app/components/ui/Product/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 export default function Card({Name, Price, Type}: CardProps) {
   return (
     <div className="bg-amber-600 w-full h-full p-2 flex items-center flex-col gap-2">
-      <div className="bg-amber-100 w-[80%] h-3/4 rounded-lg"></div>
+      <div className="bg-amber-100 w-[90%] h-3/4 rounded-lg"></div>
       <div className="bg-amber-100 w-full h-1/4">
         <h1>{Name}</h1>
         <h2>{Price}</h2>
