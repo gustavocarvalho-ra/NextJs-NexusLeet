37366ccfd932e8e830412c9c1fba16915e399ff4
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-20 20:48:00 -0300

    style: changed cursor

--- src/app/components/ui/CardUserConfg.tsx
+++ src/app/components/ui/CardUserConfg.tsx
@@ -10,7 +10,7 @@ export default function CardUserConfg({icon, TextStr, AboutStr}: CardInt) {
 
 
   return (
-    <div className="w-[32%] h-36 bg-(--mod) flex flex-col justify-center rounded-lg p-3.5 shadow-lg/20 transition duration-150 ease-in-out  hover:shadow-lg/30">
+    <div className="w-[32%] h-36 bg-(--mod) flex flex-col justify-center rounded-lg p-3.5 shadow-lg/20 transition duration-150 ease-in-out  hover:shadow-lg/30 cursor-pointer">
       <h1 className="font-black h-1/4 text-xl">{icon}</h1>
       <h2 className="font-semibold h-1/4">{TextStr}</h2>
       <h4 className="font-extralight h-1/2 text-sm">{AboutStr}</h4>
