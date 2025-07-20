56278961326b69d7bed5f53d2e73453654be2fab
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-20 20:37:52 -0300

    feat: test card

--- src/app/components/ui/Product/Card.tsx
+++ src/app/components/ui/Product/Card.tsx
@@ -3,8 +3,12 @@
 export default function Card(Name, Price, Type) {
   return (
     <div className="bg-amber-600 w-full h-full p-2 flex items-center flex-col gap-2">
-      <div className="bg-amber-100 w-[80%] h-3/4"></div>
-      <div className="bg-amber-100 w-full h-1/4"></div>
+      <div className="bg-amber-100 w-[80%] h-3/4 rounded-lg"></div>
+      <div className="bg-amber-100 w-full h-1/4">
+        <h1>{Name}</h1>
+        <h2>{Price}</h2>
+        <h3>{Type}</h3>
+      </div>
     </div>
   )
 }
