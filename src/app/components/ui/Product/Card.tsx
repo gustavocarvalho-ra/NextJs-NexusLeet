b6cd42ec8c4f0001cd7679488fc3139ce8b01d1e
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-22 21:38:27 -0300

    style: add hover effect

--- src/app/components/ui/Product/Card.tsx
+++ src/app/components/ui/Product/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export default function Card({Name, Price, Type}: CardProps) {
   return (
-    <div className="bg-(--mod) min-w-65 h-100 p-3 flex items-center flex-col gap-3 rounded-lg">
+    <div className="bg-(--mod) min-w-65 h-100 p-3 flex items-center flex-col gap-3 rounded-lg transition duration-150 ease-in-out hover:shadow-lg/30">
       <div className="bg-amber-100 w-[90%] h-3/4 rounded-lg"></div>
       <div className="bg-amber-100 w-full h-1/4">
         <h1>{Name}</h1>
