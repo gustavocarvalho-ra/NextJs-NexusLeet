3b0b0ef74cbacebb123ad607acd5289672dd2299
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-25 19:01:16 -0300

    feat: div changed to link

--- src/app/components/ui/Product/Card.tsx
+++ src/app/components/ui/Product/Card.tsx
@@ -1,19 +1,21 @@
+import Link from "next/link";
 
 interface CardProps {
+  id: number;
   Name: React.ReactNode;
   Price: React.ReactNode;
   Type: React.ReactNode;
 }
 
-export default function Card({Name, Price, Type}: CardProps) {
+export default function Card({id, Name, Price, Type}: CardProps) {
   return (
-    <div className="bg-(--mod) min-w-65 h-100 p-3 flex items-center flex-col gap-3 rounded-lg transition duration-250 ease-in-out hover:shadow-lg/40 cursor-pointer">
+    <Link href={`/products/${id}`} className="bg-(--mod) min-w-65 h-100 p-3 flex items-center flex-col gap-3 rounded-lg transition duration-250 ease-in-out hover:shadow-lg/40 cursor-pointer">
       <div className="bg-amber-100 w-[90%] h-3/4 rounded-lg"></div>
       <div className="bg-amber-100 w-full h-1/4">
         <h1>{Name}</h1>
         <h2>{Price}</h2>
         <h3>{Type}</h3>
       </div>
-    </div>
+    </Link>
   )
 }
