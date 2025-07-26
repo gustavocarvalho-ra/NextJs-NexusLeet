d091c794371a3c2a2341e6bd1e593b51ab38c1fb
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-26 19:13:37 -0300

    feat: test menu

--- src/app/pag/main/page.tsx
+++ src/app/pag/main/page.tsx
@@ -2,6 +2,7 @@
 // import MyAccont from "./my-account/page";
 import Card from './../../components/ui/Product/Card';
 import Header  from '@/app/components/Header';
+import MenuCategoria from '@/app/components/MenuCategoria';
 
 export default function Main() {
   return (
@@ -9,6 +10,7 @@ export default function Main() {
       <Header />
       <h1>Tela inicial</h1>
       <div className='w-full h-full flex justify-center'>
+        <MenuCategoria/>
 
         <div className='w-9/12 min-h-screen grid grid-cols-5 gap-5.5 flex-wrap '>
           <Card id={1} Name="test" Price="99,99" Type="test"/>
