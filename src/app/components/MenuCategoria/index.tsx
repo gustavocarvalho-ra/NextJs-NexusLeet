b09c03c168d0b948e23ed20e609387d3e987fe70
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-26 19:14:50 -0300

    test menu

--- src/app/components/MenuCategoria/index.tsx
+++ src/app/components/MenuCategoria/index.tsx
@@ -2,7 +2,7 @@
 
 export default function MenuCategoria() {
   return (
-    <div className="w-44 h-60 bg-amber-700 z-50 absolute">
+    <div className="w-44 h-60 bg-amber-700 z-50 relative">
       <h1>test Menu</h1>
     </div>
   )
