fc4080b531cc0a4a84f828dc252f61f87c302cbc
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-27 23:32:46 -0300

    feat: add variable className in component

--- src/app/components/MenuCategoria/index.tsx
+++ src/app/components/MenuCategoria/index.tsx
@@ -1,8 +1,8 @@
+import { ComponentProps } from "react"
 
-
-export default function MenuCategoria() {
+export default function MenuCategoria({ className = '', ...props }: ComponentProps<'div'>) {
   return (
-    <div className="w-44 h-60 bg-amber-700 z-50 relative">
+    <div className={`w-44 h-60 bg-amber-700 z-50 relative ${className}`} {...props}>
       <h1>test Menu</h1>
     </div>
   )
