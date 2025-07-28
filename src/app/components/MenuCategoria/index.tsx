e5e2b6d33b5073d0c045ef472a1bc3a3534ad670
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-27 23:39:54 -0300

    feat: test menu

--- src/app/components/MenuCategoria/index.tsx
+++ src/app/components/MenuCategoria/index.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from "react"
 
 export default function MenuCategoria({ className = '', ...props }: ComponentProps<'div'>) {
   return (
-    <div className={`w-44 h-60 bg-amber-700 z-50 relative ${className}`} {...props}>
+    <div className={`w-full h-60 bg-amber-700 z-50 relative ${className}`} {...props}>
       <h1>test Menu</h1>
     </div>
   )
--- src/app/pag/main/page.tsx
+++ src/app/pag/main/page.tsx
@@ -8,11 +8,10 @@ export default function Main() {
   return (
     <div className="w-screen h-screen flex flex-col">
       <Header />
-      <h1>Tela inicial</h1>
       <div className='w-full h-full flex justify-center'>
-        <MenuCategoria/>
-
         <div className='w-9/12 min-h-screen grid grid-cols-5 gap-5.5 flex-wrap '>
+        <MenuCategoria className='w-3/12'/>
+
           <Card id={1} Name="test" Price="99,99" Type="test"/>
           <Card id={1} Name="test" Price="99,99" Type="test"/>
           <Card id={1} Name="test" Price="99,99" Type="test"/>
