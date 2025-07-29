83430793d3dcb935cf1268fe21fb84e8bd2c4602
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-28 21:34:41 -0300

    style: test width

--- src/app/pag/main/page.tsx
+++ src/app/pag/main/page.tsx
@@ -8,9 +8,9 @@ export default function Main() {
   return (
     <div className="w-screen h-screen flex flex-col">
       <Header />
-      <div className='w-full h-full flex justify-center'>
-        <div className='w-9/12 min-h-screen grid grid-cols-5 gap-5.5 flex-wrap '>
-        <MenuCategoria className='w-3/12'/>
+      <div className='w-full h-full flex'>
+        <MenuCategoria className='w-1/4'/>
+        <div className='w-3/4 min-h-screen grid grid-cols-5 gap-5.5 flex-wrap '>
 
           <Card id={1} Name="test" Price="99,99" Type="test"/>
           <Card id={1} Name="test" Price="99,99" Type="test"/>
