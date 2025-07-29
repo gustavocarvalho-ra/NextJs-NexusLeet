a963ca9aa8b8e4c15e4d0482d4c50c3eb0b91741
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-28 21:43:06 -0300

    fix: adjust grid column count in main layout

--- src/app/pag/main/page.tsx
+++ src/app/pag/main/page.tsx
@@ -12,7 +12,7 @@ export default function Main() {
         <div className='w-1/4'>
           <MenuCategoria className=''/>
         </div>
-        <div className='w-3/4 min-h-screen grid grid-cols-5 gap-5.5 flex-wrap '>
+        <div className='w-3/4 min-h-screen grid grid-cols-4 gap-5.5 flex-wrap '>
 
           <Card id={1} Name="test" Price="99,99" Type="test"/>
           <Card id={1} Name="test" Price="99,99" Type="test"/>
