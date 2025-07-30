3abec83c9fe27ddd964d440a4d5fe06f317b0251
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-29 23:40:41 -0300

    fix: update grid column count to 5 in main layout

--- src/app/pag/main/page.tsx
+++ src/app/pag/main/page.tsx
@@ -15,7 +15,7 @@ export default function Main() {
         </div>
 
         <div className='w-3/4 min-h-screen'>
-          <div className='w-[90%] grid grid-cols-4 gap-5.5 flex-wrap'>
+          <div className='w-[90%] grid grid-cols-5 gap-5.5 flex-wrap'>
             <Card id={1} Name="test" Price="99,99" Type="test"/>
             <Card id={1} Name="test" Price="99,99" Type="test"/>
             <Card id={1} Name="test" Price="99,99" Type="test"/>
