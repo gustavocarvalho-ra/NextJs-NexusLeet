e4382bfe4661856f77eb519deb85cc0e7e434ea6
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-30 23:54:06 -0300

    style: change positioning of MenuCategoria component to sticky

--- src/app/components/MenuCategoria/index.tsx
+++ src/app/components/MenuCategoria/index.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from "react"
 
 export default function MenuCategoria({ className = '', ...props }: ComponentProps<'div'>) {
   return (
-    <div className={`w-[60%] h-60 bg-amber-700 z-50 relative ${className}`} {...props}>
+    <div className={`w-[60%] h-60 bg-amber-700 z-50 sticky top-35 ${className}`} {...props}>
       <h1>test Menu</h1>
     </div>
   )
