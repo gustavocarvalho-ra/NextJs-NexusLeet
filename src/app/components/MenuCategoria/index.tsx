5e77b02fa997ad11f15ea7963b441dd7ab799859
GustavoCR021 <gustavoc1922@gmail.com>
2025-07-31 23:38:38 -0300

    fix: correct header text in MenuCategoria component

--- src/app/components/MenuCategoria/index.tsx
+++ src/app/components/MenuCategoria/index.tsx
@@ -3,7 +3,7 @@ import { ComponentProps } from "react"
 export default function MenuCategoria({ className = '', ...props }: ComponentProps<'div'>) {
   return (
     <div className={`w-[60%] h-60 bg-amber-700 z-50 sticky top-35 ${className}`} {...props}>
-      <h1>test Menu</h1>
+      <h1>Menu</h1>
     </div>
   )
 }
