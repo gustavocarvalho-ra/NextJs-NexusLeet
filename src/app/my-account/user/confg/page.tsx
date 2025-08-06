af3fadfeda62ee71181add8c61d496764515781a
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-06 20:00:37 -0300

    feat: add new global color

--- src/app/my-account/user/confg/page.tsx
+++ src/app/my-account/user/confg/page.tsx
@@ -17,22 +17,22 @@ export default async function ConfigUserAccount() {
     <div className="w-screen h-screen flex flex-col">
       <Header />
       <div className="w-full h-full flex flex-col items-center justify-center gap-16">
-        <div className="w-1/3 h-1/6 bg-amber-300">
-          <h1>E-mail</h1>
-          <h3>{session.user?.email}</h3>
+        <div className="w-1/3 h-1/6 bg-(--mod)">
+          <h1 className="">E-mail</h1>
+          <h3 className="text-(--grey-text)">{session.user?.email}</h3>
         </div>
 
         
-        <div className="w-1/3 h-1/6 bg-amber-300">
-          <h1>Nome de usuário</h1>
-          <h3>{session.user?.name}</h3>
+        <div className="w-1/3 h-1/6 bg-(--mod)">
+          <h1 className="">Nome de usuário</h1>
+          <h3 className="text-(--grey-text)">{session.user?.name}</h3>
           <Link href={''}></Link>
 
         </div>
-        <div className="w-1/3 h-1/6 bg-amber-300 relative">
-          <h1>Alter senha</h1>
-          <Link href={'/'} className="bg-(--text-amber) w-[10%] h-[5%]">
-            <Button className="w-1/12 h-1/12 bottom-10 border-none">
+        <div className="w-1/3 h-1/6 bg-(--mod) relative">
+          <h1 className="">Alter senha</h1>
+          <Link href={'/'} className="bg-(--text-amber) w-[10%] h-full">
+            <Button className="w-1/12 h-1/2 bottom-10 border-none">
               Alterar
             </Button>
           </Link>
