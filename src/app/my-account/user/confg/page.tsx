37b084e0d822ff850368031e62d8ff1dcd72be57
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-05 23:07:28 -0300

    fix: correct redirect logic in ConfigUserAccount function

--- src/app/my-account/user/confg/page.tsx
+++ src/app/my-account/user/confg/page.tsx
@@ -8,17 +8,17 @@ export default async function ConfigUserAccount() {
   const session = await getServerSession(authOptions)
 
   if (!session) {
+    redirect("/my-account/user");
+  }
+
+  return (
     <div className="w-screen h-screen flex flex-col">
       <Header />
       <div className="w-full h-full flex flex-col items-center justify-center gap-16">
         <div className="w-1/2 h-1/6 bg-amber-300"><h1>test card</h1></div>
         <div className="w-1/2 h-1/6 bg-amber-300"><h1>test card</h1></div>
         <div className="w-1/2 h-1/6 bg-amber-300"><h1>test card</h1></div>
-
       </div>
     </div>
-
-  }
-
-  redirect("/my-account/user");
+  )
 }
