058a66cca9c2ce7fec4b271b0f1034386a5933c2
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-05 21:27:32 -0300

    refactor: streamline error handling and response structure in change-password route

--- src/app/api/auth/change-password/route.ts
+++ src/app/api/auth/change-password/route.ts
@@ -5,36 +5,43 @@ import { NextResponse } from "next/server";
 import { hash, compare } from "bcrypt";
 
 export async function POST(req: Request) {
-  const session = await getServerSession(authOptions);
-  if (!session || !session.user?.email) {
-    return NextResponse.json({ error: "Não autorizado" }, {status: 401});
+  try {
+
+    const session = await getServerSession(authOptions);
+    if (!session || !session.user?.email) {
+      return NextResponse.json({ error: "Não autorizado" }, {status: 401});
+    }
+  
+    const { currentPassword, newPassword } = await req.json();
+  
+    if (!currentPassword || !newPassword) {
+      return NextResponse.json({ error: "Dados invalidos" }, {status: 400});
+    }
+  
+    const user = await prisma.user.findUnique({
+      where: { email: session.user.email },
+    });
+  
+    if (!user || !user.password) {
+      return NextResponse.json({ error: "Usuário não encontrado" }, {status: 400});
+    }
+  
+    const isValid = await compare(currentPassword, user.password);
+    if (!isValid) {
+      return NextResponse.json({ error: "Senha atual incorrenta" }, { status: 400 });
+    }
+  
+    const hashedNewPassword = await hash(newPassword, 10);
+  
+    await prisma.user.update({
+      where: { email: session.user.email },
+      data: { password: hashedNewPassword },
+    });
+  
+  
+    return NextResponse.json({ message: "Senha alterada com sucesso" });
+  } catch (error) {
+    console.log("[CHANGE_PASSWORD_ERROR]", error);
+    return NextResponse.json({ error: "Erro interno do servidor."}, {status: 500})
   }
-
-  const { currentPassword, newPassword } = await req.json();
-
-  if (!currentPassword || !newPassword) {
-    return NextResponse.json({ error: "Dados invalidos" }, {status: 400});
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { email: session.user.email },
-  });
-
-  if (!user || !user.password) {
-    return NextResponse.json({ error: "Usuário não encontrado" }, {status: 400});
-  }
-
-  const isValid = await compare(currentPassword, user.password);
-  if (!isValid) {
-    return NextResponse.json({ error: "Senha atual incorrenta" }, { status: 400 });
-  }
-
-  const hashedNewPassword = await hash(newPassword, 10);
-
-  await prisma.user.update({
-    where: { email: session.user.email },
-    data: { password: hashedNewPassword },
-  });
-
-  return NextResponse.json({ message: "Senha alterada com sucesso" });
 }
