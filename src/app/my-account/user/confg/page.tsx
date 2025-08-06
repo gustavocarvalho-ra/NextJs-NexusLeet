ce0f91cd4c44572bfaaae8fa8623566a2ad4601c
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-05 23:02:55 -0300

    test card

--- src/app/my-account/user/confg/page.tsx
+++ src/app/my-account/user/confg/page.tsx
@@ -10,7 +10,10 @@ export default async function ConfigUserAccount() {
   if (!session) {
     <div className="w-screen h-screen flex flex-col">
       <Header />
-      <div className="w-full h-full">
+      <div className="w-full h-full flex flex-col items-center justify-center gap-16">
+        <div className="w-1/2 h-1/6 bg-amber-300"><h1>test card</h1></div>
+        <div className="w-1/2 h-1/6 bg-amber-300"><h1>test card</h1></div>
+        <div className="w-1/2 h-1/6 bg-amber-300"><h1>test card</h1></div>
 
       </div>
     </div>
