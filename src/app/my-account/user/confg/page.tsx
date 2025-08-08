4b99241475b0ceda0fb0e7899a7b61138c31b51b
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-07 22:08:42 -0300

    style: add border radius

--- src/app/my-account/user/confg/page.tsx
+++ src/app/my-account/user/confg/page.tsx
@@ -20,7 +20,7 @@ export default async function ConfigUserAccount() {
         <div className="w-1/3 h-1/6 bg-(--mod) gap-1 flex flex-col p-4 rounded-sm relative">
           <h1 className="font-medium">E-mail</h1>
           <h3 className="text-(--grey-text) text-sm">{session.user?.email}</h3>
-          <Link href={'/'} className="bg-(--text-amber) w-[10%] h-1/4 relative">
+          <Link href={'/'} className="bg-(--text-amber) w-[10%] h-1/4 rounded-lg relative">
             <Button className="w-full h-full border-none font-light">
               Alterar
             </Button>
@@ -32,7 +32,7 @@ export default async function ConfigUserAccount() {
         <div className="w-1/3 h-1/6 bg-(--mod) gap-1 flex flex-col p-4 rounded-sm relative">
           <h1 className="font-medium">Nome de usuário</h1>
           <h3 className="text-(--grey-text) text-sm">{session.user?.name}</h3>
-          <Link href={'/'} className="bg-(--text-amber) w-[10%] h-1/4 relative">
+          <Link href={'/'} className="bg-(--text-amber) w-[10%] h-1/4 rounded-lg relative">
             <Button className="w-full h-full border-none font-light">
               Alterar
             </Button>
@@ -41,7 +41,7 @@ export default async function ConfigUserAccount() {
         </div>
         <div className="w-1/3 h-1/6 bg-(--mod) gap-1 flex flex-col p-4 rounded-sm relative">
           <h1 className="">Alter senha</h1>
-          <Link href={'/'} className="bg-(--text-amber) w-[10%] h-1/4 relative">
+          <Link href={'/'} className="bg-(--text-amber) w-[10%] h-1/4 rounded-lg relative">
             <Button className="w-full h-full border-none font-light">
               Alterar
             </Button>
