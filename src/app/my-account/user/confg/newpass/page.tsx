49427d55af8524bdd8c860f579605e438ac07eef
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-08 22:08:10 -0300

    feat: create ChangePasswordForm component for user password management

--- src/app/my-account/user/confg/newpass/page.tsx
+++ src/app/my-account/user/confg/newpass/page.tsx
@@ -1,42 +1,18 @@
-"use client"
 
-import { useState } from "react";
+import Header from "@/app/components/Header";
+import ChangePasswordForm from "@/app/components/login/useConfig/NewPass";
 
-export default function ChangePasswordForm() {
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [message, setMessage] = useState("");
-
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    const res = await fetch("/api/auth/userConfig/change-password", {
-      method: "POST",
-      body: JSON.stringify({ currentPassword, newPassword }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    const data = await res.json();
-    setMessage(data.message || data.error);
-  }
+export default function NewPass() {
 
   return (
-    <form onSubmit={handleSubmit} className="absolute">
-      <input
-        type="password" 
-        placeholder="Senha atual"
-        value={currentPassword}
-        onChange={(e) => setCurrentPassword(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Nova senha"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-      />
-      <button type="submit" className="bg-blue-600 text-amber-400 rounded-lg">
-        Alterar senha
-      </button>
-      {message && <p>{message}</p>}
-    </form>
+    <div className="w-scren h-scren">
+      <Header />
+
+    <div>
+      <ChangePasswordForm />
+    </div>
+
+    </div>
+
   );
 }
