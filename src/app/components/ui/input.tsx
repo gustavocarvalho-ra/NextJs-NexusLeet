42686c49329359a886e68fb7a5d6769959a4afd6
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-09 19:10:00 -0300

    feat: add id and name in input

--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -3,11 +3,17 @@ import React, { InputHTMLAttributes } from "react";
 export type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, id, name, ...props }, ref) => {
+
+    const genereteId = React.useId();
+    const inputId = id || name || genereteId;
+
     return (
       <input
         ref={ref}
         type={type}
+        id={inputId}
+        name={name || inputId}
         className={`bg-(--color-sec) rounded-sm p-2 h-full w-full focus:outline-0 ${className}`}
         {...props}
       />
