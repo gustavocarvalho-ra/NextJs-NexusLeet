43662338cb500ce3175a144c9530cedfb6160344
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-08 22:56:27 -0300

    feat: added route redirection function

--- src/app/my-account/user/confg/newpass/page.tsx
+++ src/app/my-account/user/confg/newpass/page.tsx
@@ -1,8 +1,16 @@
 
 import Header from "@/app/components/Header";
 import ChangePasswordForm from "@/app/components/login/useConfig/NewPass";
+import { authOptions } from "@/app/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function NewPass() {
+export default async function NewPass() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect("/my-account");
+  }
 
   return (
     <div className="w-scren h-scren">
