e72058094695b6efb4d52210db8fdbb472cdc42a
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-08 22:33:58 -0300

    feat: add confirm password

--- src/app/components/login/useConfig/NewPass.tsx
+++ src/app/components/login/useConfig/NewPass.tsx
@@ -3,17 +3,24 @@
 import { useState } from "react";
 
 export default function ChangePasswordForm() {
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
+  // const [currentPassword, setCurrentPassword] = useState("");
+  // const [newPassword, setNewPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [form, setForm] = useState({ currentPassword: "", newPassword: "", confirmNewPassword: ""});
+  const [error, setError] = useState("");
 
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     const res = await fetch("/api/auth/userConfig/change-password", {
       method: "POST",
-      body: JSON.stringify({ currentPassword, newPassword }),
+      body: JSON.stringify(form),
       headers: { "Content-Type": "application/json" },
     });
+
+    if (form.newPassword !== form.confirmNewPassword) {
+      setError("As senhas não coincidem!")
+      return;
+    }
 
     const data = await res.json();
     setMessage(data.message || data.error);
@@ -24,14 +31,14 @@ export default function ChangePasswordForm() {
       <input
         type="password" 
         placeholder="Senha atual"
-        value={currentPassword}
-        onChange={(e) => setCurrentPassword(e.target.value)}
+        value={form.currentPassword}
+        onChange={(e) => setForm({...form, currentPassword: e.target.value})}
       />
       <input
         type="password"
         placeholder="Nova senha"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
+        value={form.newPassword}
+        onChange={(e) => setForm({...form, newPassword: e.target.value})}
       />
       <button type="submit" className="bg-blue-600 text-amber-400 rounded-lg">
         Alterar senha
