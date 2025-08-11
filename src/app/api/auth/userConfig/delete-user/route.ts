54bcbc78de9e234df57944f8a9eced468ed2d557
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-11 20:41:01 -0300

    feat: changed router logic

--- src/app/api/auth/userConfig/delete-user/route.ts
+++ src/app/api/auth/userConfig/delete-user/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/app/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/lib/auth";
+import { compare } from "bcrypt";
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
@@ -17,6 +18,19 @@ export async function POST(req: Request) {
 
   const user = await prisma.user.findUnique({
     where: { email: session.user.email },
-  })
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: "Usuário não encontrado" }, { status: 404 });
   }
+
+  const isValid = await compare(password, user.password);
+  if (!isValid) {
+    return NextResponse.json({ error: "Senha incorreta" }, { status: 403 });
+  }
+
+  await prisma.user.delete({
+    where: { id: user.id}
+  });
+  return NextResponse.json({ success: true });
 }
