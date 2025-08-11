7bb769672580d363279539df9a4e87a7d67c6f09
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-11 20:49:15 -0300

    feat: changed component

--- src/app/components/login/useConfig/DeleteUser.tsx
+++ src/app/components/login/useConfig/DeleteUser.tsx
@@ -1,13 +1,32 @@
-export async function handleDeleteUser() {
-  const res = await fetch("/api/auth/userConfig/delete-user", {
-    method: "DELETE",
-  });
+"use client"
 
-  if (res.ok) {
-    console.log("Usuário deletado");
-  } else {
-    console.error("erro ao deletar");
+import { useState } from "react";
+
+export function DeleteAccountModal({ onClose }: { onClose: () => void }) {
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  async function handleDeleteUser() {
+    setLoading(true);
+    setError("");
+
+    const res = await fetch("/api/auth/userConfig/delete-user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ password }),
+    });
+
+    const data = await res.json()
+
+    if (res.ok) {
+      console.log("Usuário deletado");
+    } else {
+      console.error("erro ao deletar");
+    }
   }
+
+
 
   return (
     <button onClick={handleDeleteUser}>
