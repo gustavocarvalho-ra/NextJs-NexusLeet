63e473e4544c7c14f8cc00d123e33d0487d1a8f9
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-10 21:20:41 -0300

    feat: test function delete-user

--- src/app/components/login/useConfig/DeleteUser.tsx
+++ src/app/components/login/useConfig/DeleteUser.tsx
@@ -1,7 +1,17 @@
-async function handleDeleteUser() {
+export async function handleDeleteUser() {
   const res = await fetch("/api/auth/userConfig/delete-user", {
     method: "DELETE",
   });
 
-  
+  if (res.ok) {
+    console.log("Usuário deletado");
+  } else {
+    console.error("erro ao deletar");
+  }
+
+  return (
+    <button onClick={handleDeleteUser}>
+      Deletar usuário
+    </button>
+  )
 }
