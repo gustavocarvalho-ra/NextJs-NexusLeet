bb3e940dd9260dc30da2e62f3537b52130e8b7af
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-17 21:36:46 -0300

    feat: add div content

--- src/app/components/login/useConfig/NewPass.tsx
+++ src/app/components/login/useConfig/NewPass.tsx
@@ -32,34 +32,39 @@ export default function ChangePasswordForm() {
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="h-1/2 w-1/4 bg-slate-500/15 border-2 border-(--text-amber) p-5 rounded-lg flex flex-col justify-center">
-        <form onSubmit={handleSubmit} className="w-full h-full flex flex-col gap-3 items-center relative" name="formNewPass">
+        <form onSubmit={handleSubmit} className="w-full h-full flex flex-col gap-3 items-center" name="formNewPass">
           <h1 className="font-semibold text-3xl text-(--text-amber)">Alterar Senha</h1>
+          
+          <div className="h-full relative">
+            <Input
+              type="password" 
+              placeholder="Senha atual"
+              value={form.currentPassword}
+              onChange={(e) => setForm({...form, currentPassword: e.target.value})}
+              className="h-1.5"
+            />
 
-          <Input
-            type="password" 
-            placeholder="Senha atual"
-            value={form.currentPassword}
-            onChange={(e) => setForm({...form, currentPassword: e.target.value})}
-          />
+            <Input
+              type="password"
+              placeholder="Nova senha"
+              value={form.newPassword}
+              onChange={(e) => setForm({...form, newPassword: e.target.value})}
+            />
 
-          <Input
-            type="password"
-            placeholder="Nova senha"
-            value={form.newPassword}
-            onChange={(e) => setForm({...form, newPassword: e.target.value})}
-          />
+            <Input
+              type="password"
+              placeholder="Confirme a nova senha"
+              value={form.confirmNewPassword}
+              onChange={(e) => setForm({...form, confirmNewPassword: e.target.value})}
+            />
 
-          <Input
-            type="password"
-            placeholder="Confirme a nova senha"
-            value={form.confirmNewPassword}
-            onChange={(e) => setForm({...form, confirmNewPassword: e.target.value})}
-          />
-          <Button type="submit" className=" rounded-lg w-1/2">
-            Alterar senha
-          </Button>
-          {message && <p>{message}</p>}
-          {error && <p>{error}</p>}
+            <Button type="submit" className=" rounded-lg w-1/2">
+              Alterar senha
+            </Button>
+
+            {message && <p>{message}</p>}
+            {error && <p>{error}</p>}
+          </div>
         </form>
       </div>
     </div>
