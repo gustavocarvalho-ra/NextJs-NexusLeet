16caf6ddd416c7b75cb4f69817cf6483deb62507
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-20 10:58:48 -0300

    style: add div content and display

--- src/app/my-account/user/confg/delete-acount/page.tsx
+++ src/app/my-account/user/confg/delete-acount/page.tsx
@@ -3,15 +3,15 @@ import DeleteAccountModal from "@/app/components/login/useConfig/DeleteUser"
 
 export default function DeletAcount() {
   return (
-    <>
+    <div className="w-screen h-screen">
       <Header />
 
-      <div className="w-full h-full">
-        <h1>Quer mesmo deletar a conta? :(</h1>
-        <div>
+      <div className="w-full h-full flex flex-col justify-center items-center ">
+        <h1>Quer mesmo deletar sua conta? :(</h1>
+        <div className="w-full flex justify-center">
           <DeleteAccountModal />
         </div>
       </div>
-    </>
+    </div>
   )
 }
