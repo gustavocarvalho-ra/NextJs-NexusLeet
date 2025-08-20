bd93a57768bf6e98f9843c2cfe9f565cbe450322
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-19 22:42:44 -0300

    feat: add new page

---
+++ src/app/my-account/user/confg/delete-acount/page.tsx
@@ -0,0 +1,12 @@
+import DeleteAccountModal from "@/app/components/login/useConfig/DeleteUser"
+
+export default function DeletAcount() {
+  return (
+    <div>
+      <h1>Quer mesmo deletar a conta? :(</h1>
+      <div>
+        <DeleteAccountModal />
+      </div>
+    </div>
+  )
+}
