d5ec0468a60c0fe3335302f2cdb7baa240415d02
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-22 23:49:23 -0300

    style: changed font weight

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -57,12 +57,12 @@ export default async function Header() {
 
           modalContent={
             <div className='font-medium text-(--inv-sec) flex flex-col w-full h-full gap-3'>
-              <div className='flex w-full h-1/4 gap-1.5 items-center'>
+              <div className='flex w-full h-1/4 gap-1.5 items-center font-semibold'>
                 <FaRegUser className='text-2xl' />
                 <p>{session.user?.name}</p>
               </div>
                 
-              <div className='w-full h-full flex flex-col gap-1.5 '>
+              <div className='w-full h-full flex flex-col gap-1.5 font-light'>
                 <Link href={"/my-account/user"} className='hover:text-(--text-amber)'>Meu perfil</Link>
                 <ButtonSignOut className='flex cursor-pointer hover:text-(--text-amber)' />
               </div>
