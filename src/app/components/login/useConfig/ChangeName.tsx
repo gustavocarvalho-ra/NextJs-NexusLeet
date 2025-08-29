0c624744d5f23b1e43ef4cf234d9bd2f1b041018
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-28 22:26:24 -0300

    feat: implement name update functionality

--- src/app/components/login/useConfig/ChangeName.tsx
+++ src/app/components/login/useConfig/ChangeName.tsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+
 export default function UpdateName() {
-  
+  const [newName, setNewName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: React.FocusEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/auth/userConfig/change-name", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ newName }),
+      });
+    }
+
+    const data = await res.json();
+
+    if (res.ok) {
+      setMessage("Nome atualidado com sucesso!");
+      setNewName("");
+    } else {
+      setMessage(data.error || "Erro ao atualizar nome")
+    }
+  }
 }
