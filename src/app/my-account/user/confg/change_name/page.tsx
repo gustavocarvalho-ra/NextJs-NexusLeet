b15b341c778b098bd1b5accd711159a9634ab4c5
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-29 19:52:44 -0300

    fix: improve layout styling for UpdateName page

--- src/app/my-account/user/confg/change_name/page.tsx
+++ src/app/my-account/user/confg/change_name/page.tsx
@@ -5,7 +5,7 @@ export default function UpdateNamePag() {
   return (
     <div className="w-screen h-screen">
       <Header />
-      <div>
+      <div className="w-full h-full flex flex-col justify-center items-center">
 
         <UpdateName />
       </div>
