76072302b36a60464ca366a9daebf4001b7bc99c
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-29 19:51:40 -0300

    feat: add Header component

--- src/app/my-account/user/confg/change_name/page.tsx
+++ src/app/my-account/user/confg/change_name/page.tsx
@@ -1,7 +1,14 @@
+import Header from "@/app/components/Header";
 import UpdateName from "@/app/components/login/useConfig/ChangeName";
 
 export default function UpdateNamePag() {
   return (
-    <UpdateName />
+    <div className="w-screen h-screen">
+      <Header />
+      <div>
+
+        <UpdateName />
+      </div>
+    </div>
   )
 }
