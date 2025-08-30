1f8a5c060d2e64a0b0fd6397f9ac0a755e5a5d89
GustavoCR021 <gustavoc1922@gmail.com>
2025-08-30 20:31:02 -0300

    style: add border radius

--- src/app/components/login/useConfig/ChangeName.tsx
+++ src/app/components/login/useConfig/ChangeName.tsx
@@ -31,7 +31,7 @@ export default function UpdateName() {
   };
   
   return (
-    <Form className="w-1/4 h-1/3 bg-slate-500/15 flex flex-col items-center justify-center gap-5 relative" action={handleSubmit}>
+    <Form className="w-1/4 h-1/3 bg-slate-500/15 flex flex-col items-center justify-center gap-5 relative rounded-lg" action={handleSubmit}>
       <h1 className="font-semibold text-2xl absolute top-8">Alterar nome de usuário</h1>
       <input
         type="text" 
