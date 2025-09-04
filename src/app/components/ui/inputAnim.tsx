8d17583021cc3621bbe15377c5818b3dbc0b8131
GustavoCR021 <gustavoc1922@gmail.com>
2025-09-03 22:46:03 -0300

    style: test className

--- src/app/components/ui/inputAnim.tsx
+++ src/app/components/ui/inputAnim.tsx
@@ -1,4 +1,7 @@
 <div>
-  <input type="text" />
+  <input 
+    type="text"
+    className="ease focus:border-50 peer w-full rouded-md border border-white bg-transparent px-3 py-3 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-50 hover:border-slate-300 focus:outline-none"
+  />
   <label htmlFor=""></label>
 </div>
