b9e50b71f4d50e6c0c5d052417d622374e067d28
GustavoCR021 <gustavoc1922@gmail.com>
2025-09-03 23:31:47 -0300

    refactor: update InputAt component styles for consistency and clarity

--- src/app/components/ui/inputAnim.tsx
+++ src/app/components/ui/inputAnim.tsx
@@ -3,14 +3,15 @@ export default function InputAt() {
   return (
     <div className="relative">
       <input 
+        required
         id="input"
         name="input"
         type="text"
-        className="ease focus:border-50 peer w-full rouded-md border border-white bg-transparent px-3 py-3 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-50 hover:border-slate-300 focus:outline-none"
+        className="px-4 py-2 text-lg outline-none border-2 border-gray-400 rounded hover:border-gray-600 duration-200 peer focus:border-red-700 bg-inherit"
       />
       <label 
         htmlFor="input"
-        className="pointer-events-none absolute left-2.5 top-2 origin-left transform cursor-text bg-amber-800 px-1 text-sm text-red transition-all duration-150 peer-focus:-top2 peer-focus:left-2.5 peer-focus:scale-90 peer-focus:text-xs peer-focus:text-slate-50"
+        className="absolute left-0 top-2 px-1 text-lg tracking-wide peer-focus:text-red-600 pointer-events-none duration-200 peer-focus:text-sm peer-focus:-translate-y-5 bg-gray-200 ml-2 peer-valid:text-sm peer-valid:-translate-y-5"
       >
         test input
       </label>
