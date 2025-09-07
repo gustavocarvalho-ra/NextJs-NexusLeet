a65defff41886ef944af74f06d920f1055539765
GustavoCR021 <gustavoc1922@gmail.com>
2025-09-06 21:39:21 -0300

    refactor: convert InputAt to a forwardRef component for improved flexibility

--- src/app/components/ui/inputAnim.tsx
+++ src/app/components/ui/inputAnim.tsx
@@ -1,20 +1,34 @@
+import React, { InputHTMLAttributes } from 'react';
 
-export default function InputAt() {
-  return (
-    <div className="relative">
-      <input 
-        required
-        id="input"
-        name="input"
-        type="text"
-        className="px-4 py-2 text-lg outline-none border-2 border-gray-400 rounded hover:border-gray-600 duration-200 peer focus:border-red-700 bg-inherit"
-      />
-      <label 
-        htmlFor="input"
-        className="absolute left-0 top-2 px-1 text-lg tracking-wide peer-focus:text-red-600 pointer-events-none duration-200 peer-focus:text-sm peer-focus:-translate-y-5 bg-gray-200 ml-2 peer-valid:text-sm peer-valid:-translate-y-5"
-      >
-        test input
-      </label>
-    </div>
-  )
-}
+export type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+const InputAt = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, id, name, ...props }, ref) => {
+    
+    const genereteId = React.useId();
+    const inputId = id || name || genereteId;
+
+    return (
+      <div className="relative">
+        <input 
+          required
+          ref={ref}
+          id={id}
+          name={name || inputId}
+          type={type}
+          className={`px-4 py-2 text-lg outline-none border-2 border-gray-400 rounded hover:border-gray-600 duration-200 peer focus:border-red-700 bg-inherit ${className}`}
+          {...props}
+        />
+        <label 
+          htmlFor={id}
+          className={`absolute left-0 top-2 px-1 text-lg tracking-wide peer-focus:text-red-600 pointer-events-none duration-200 peer-focus:text-sm peer-focus:-translate-y-5 bg-gray-200 ml-2 peer-valid:text-sm peer-valid:-translate-y-5 ${className}`}
+        >
+          test input
+        </label>
+      </div>
+    )
+});
+
+InputAt.displayName = "Input";
+
+export { InputAt };
