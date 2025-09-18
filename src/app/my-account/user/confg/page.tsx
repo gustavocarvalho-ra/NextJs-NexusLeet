c31bcdc36fdab5f01a1d6fd6a0a98022faf1779d
GustavoCR021 <gustavoc1922@gmail.com>
2025-09-17 22:37:21 -0300

    style: remove grey text color from email and username display

--- src/app/my-account/user/confg/page.tsx
+++ src/app/my-account/user/confg/page.tsx
@@ -19,7 +19,7 @@ export default async function ConfigUserAccount() {
       <div className="w-full h-full flex flex-col items-center justify-center gap-16">
         <div className="w-1/3 h-1/6 bg-(--mod) gap-1 flex flex-col p-4 rounded-sm relative">
           <h1 className="font-medium">E-mail</h1>
-          <h3 className="text-(--grey-text) text-sm">{session.user?.email}</h3>
+          <h3 className="text-sm">{session.user?.email}</h3>
           <Link href={'/'} className="bg-(--text-amber) w-[10%] h-1/4 rounded-lg relative">
             <Button className="w-full h-full border-none font-light">
               Alterar
@@ -29,7 +29,7 @@ export default async function ConfigUserAccount() {
         
         <div className="w-1/3 h-1/6 bg-(--mod) gap-1 flex flex-col p-4 rounded-sm relative">
           <h1 className="font-medium">Nome de usuário</h1>
-          <h3 className="text-(--grey-text) text-sm">{session.user?.name}</h3>
+          <h3 className="text-sm">{session.user?.name}</h3>
           <Link href={'/my-account/user/confg/change_name'} className="bg-(--text-amber) w-[10%] h-1/4 rounded-lg relative">
             <Button className="w-full h-full border-none font-light">
               Alterar
