27c8f025c7750bcae1f2421e735f94f709b03869
GustavoCR021 <gustavoc1922@gmail.com>
2025-09-27 22:49:49 -0300

    refactor: comment out unused DivInputIcon component code

--- src/app/components/ui/DivInputIcon.tsx
+++ src/app/components/ui/DivInputIcon.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import { Input } from "./Input";
+// component in hiato
 
-interface DivInputIconProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  icon: React.ComponentType<{ className?: string }>;
-}
-
-const DivInputIcon = React.forwardRef<HTMLInputElement, DivInputIconProps>(
-  ({ className, icon: IconComponent, ...props }, ref) => {
-    return (
-      <div className="border-b-2 hover:border-(--text-amber) bg-(--color-sec) rounded-sm p-2 h-11 w-2/3 flex items-center">
-        <Input
-          ref={ref}
-          className={className}
-          {...props}
-        />
-        <IconComponent className="text-2xl hover:text-(--text-amber) cursor-pointer" />
-      </div>
-    );
-  }
-);
-
-DivInputIcon.displayName = "DivInputIcon";
-
-export { DivInputIcon };
+//import React from "react";
+//import { Input } from "./Input";
+//
+//interface DivInputIconProps extends React.InputHTMLAttributes<HTMLInputElement> {
+//  icon: React.ComponentType<{ className?: string }>;
+//}
+//
+//const DivInputIcon = React.forwardRef<HTMLInputElement, DivInputIconProps>(
+//  ({ className, icon: IconComponent, ...props }, ref) => {
+//    return (
+//      <div className="border-b-2 hover:border-(--text-amber) bg-(--color-sec) rounded-sm p-2 h-11 w-2/3 flex items-center">
+//        <Input
+//          ref={ref}
+//          className={className}
+//          {...props}
+//        />
+//        <IconComponent className="text-2xl hover:text-(--text-amber) cursor-pointer" />
+//      </div>
+//    );
+//  }
+//);
+//
+//DivInputIcon.displayName = "DivInputIcon";
+//
+//export { DivInputIcon };
